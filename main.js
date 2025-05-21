31d7f3984fea1592b5917f19f349b8001bfc862b
GUILHERME HENRIQUE DE JESUS OLIVEIRA <guilherme.hjoliveira@senacsp.edu.br>
2025-05-21 19:12:17 -0300

    estilizar a página de nota

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
 const path = require('node:path')
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1080,
+    height: 900,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
     }
   })
 
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
   win.loadFile('./src/views/index.html')
 }
 
